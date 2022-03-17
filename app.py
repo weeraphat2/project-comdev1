608fc679d244272071176e22bdbe6b295afd8057
weeraphat2 <71428561+weeraphat2@users.noreply.github.com>
2022-03-17 08:59:29 +0700

    Edit file byNanthana,Thanatchaporn,Weeraphat,Pulis

--- app.py
+++ app.py
@@ -4,34 +4,30 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'werty uiop'
 
 @app.route('/')
-def home():
-    return render_template("home.html")
+def index():
+    return render_template("index.html")
 
 @app.route('/login', methods=['POST','GET'])
 def login():
     return render_template("login.html")
 
-@app.route('/success', methods=['POST'])
-def success():
+@app.route('/home', methods=['POST'])
+def home():
     if request.method == 'POST':
         session['email'] = request.form['email']
-    return render_template("success.html")
+    return render_template("home.html")
 
 @app.route('/logout')
 def logout():
     if 'email' in session:
         session.pop('email', None)
-        return render_template("logout.html")
+        return render_template("index.html")
     else:
         return '<div class="alert alert-danger" role="alert"> A simple danger alert—check it out!</div>'
 
-@app.route('/profile')
-def profile():
-    if 'email' in session:
-        email = session['email']
-        return render_template("profile.html", email=email)
-    else:
-        return '<div class="alert alert-danger" role="alert"> A simple danger alert—check it out!</div>'
+@app.route('/signup', methods=['POST','GET'])
+def signup():
+        return render_template("home.html")
 
 
 if __name__ == "__main__" :
